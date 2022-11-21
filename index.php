f61d1d8b25025fef1d9f57e6ea2c8e20203c5141
Veltys <veltys@veltys.es>
2022-11-21 02:23:41 +0100

    ADD: Just in case $_SERVER['REMOTE_ADDR'] would be undefined

--- index.php
+++ index.php
@@ -5,14 +5,19 @@
  * @file:			index.php
  * @brief:			Quick PHP script to use in my other apps
  * @author:			Veltys
- * @date:			2022-06-07
- * @version:		1.0.0
+ * @date:			2022-11-21
+ * @version:		1.0.1
  * @note:			Usage: Put on a webserver an visit its URL
  */
 
 
 if(!isset($_REQUEST['format']) || $_REQUEST['format'] == 'txt') {
-    echo($_SERVER['REMOTE_ADDR']);
+    if(isset($_SERVER['REMOTE_ADDR'])) {
+        echo($_SERVER['REMOTE_ADDR']);
+    }
+    else {
+        echo("0.0.0.0");
+    }
 }
 else if($_REQUEST['format'] == 'html') {
     echo(<<<"EOS"
@@ -23,7 +28,18 @@ else if($_REQUEST['format'] == 'html') {
         <title>IP address</title>
     </head>
     <body>
-        <p>${_SERVER['REMOTE_ADDR']}</p>
+        <p>
+EOS);
+
+    if(isset($_SERVER['REMOTE_ADDR'])) {
+        echo($_SERVER['REMOTE_ADDR']);
+    }
+    else {
+        echo("0.0.0.0");
+    }
+
+    echo(<<<"EOS"
+</p>
     </body>
 </html>
 EOS);
